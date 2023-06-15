adc292ef3daadceb5b9d24742cdb4db6ac90b5f2
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-14 21:42:41 -0400

    Fix error in test text and complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, reps) {
+    if (reps < 0) {
+        return 'ERROR'
+    }
+    let repeatedString = '';
+    for (let rep = 1; rep <= reps; rep++) {
+        repeatedString += string;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
