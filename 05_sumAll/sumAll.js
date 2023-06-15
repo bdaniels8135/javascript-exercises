f44afcd90f39b069b3736d8093a9702e061ab361
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-14 23:19:34 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(input1, input2) {
+    if (typeof input1 !== 'number' || input1 < 0 || typeof input2 !== 'number' || input2 < 0) {
+        return 'ERROR';
+    }
+    let lowerBound, upperBound;
+    if (input1 <= input2) {
+        lowerBound = input1;
+        upperBound = input2;
+    } else {
+        lowerBound = input2;
+        upperBound = input1;
+    }
+    let sum = 0;
+    for (let summand = lowerBound; summand <= upperBound; summand++) {
+        sum += summand;
+    }
+    return sum;
 };
 
 // Do not edit below this line
