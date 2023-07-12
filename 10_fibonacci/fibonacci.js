18306c70ab48c2ed1201ce0d01b5f2d63cb38c1b
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-07-12 11:12:50 -0400

    Complete exercise 10 per instructions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const index = Number(input);
+    if (index > 2 ) {
+        return fibonacci(index - 1) + fibonacci(index - 2);
+    }
+    if (index === 1 || index === 2) {
+        return 1;
+    }
+    if (index < 1) {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
