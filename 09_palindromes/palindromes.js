38c946da4567ffba3719f33af1be291f06beb5b3
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-07-12 10:55:31 -0400

    Complete exercise 9 per instructions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const punctuationRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    const spacesRE = /\s+/g;
+    const strippedString = str.replace(punctuationRE, '').replace(spacesRE, '').toLowerCase();
+    const reversedString = strippedString.split('').reverse().join('')
+    return strippedString === reversedString;
 };
 
 // Do not edit below this line
